ec7f16351e71b289756c7f8c6a846336442107f9
Cory Schires <coryschires@gmail.com>
2012-02-11 18:00:58 -0600

    Initial commit. Installed rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roofless::Application.config.secret_token = 'e4139d7431579c980078d16d5da3c63a40fd3b6ec5b8e2131863d9fb2c08a5ea462241cfefc3748d30b6ec8f3f026dfc1ced5da009a68a29c0e61915338406a0'
