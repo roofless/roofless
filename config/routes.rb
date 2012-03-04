bd0caf24fca07b88e94f7f9c0e9e5b0b7bdf3d6e
Cory Schires <coryschires@gmail.com>
2012-03-04 13:50:29 -0600

    Cleaner url for the admin dashboard.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Roofless::Application.routes.draw do
+
+  # Redirect the homepage to the tumblr blog
   match "/" => redirect("http://rooflessrex.tumblr.com/")
 
+  # Cleaner urls for administrative stuff
   match "/login" => redirect("/users/login")
+  match "/admin" => redirect("/refinery")
 end
