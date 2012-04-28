bef6c1563f9dd98938ed6ed3a4306596fe57f09b
Cory Schires <coryschires@gmail.com>
2012-04-28 13:30:41 -0500

    Force most recently created product to be in first position.

--- vendor/engines/products/app/models/product.rb
+++ vendor/engines/products/app/models/product.rb
@@ -3,6 +3,18 @@ class Product < ActiveRecord::Base
   acts_as_indexed :fields => [:catalog_number, :name, :artist, :description, :paypal_embed_code]
 
   validates :catalog_number, :presence => true, :uniqueness => true
-  
+
   belongs_to :image
+
+  after_create :force_product_to_be_in_first_position!
+
+  private
+
+  def force_product_to_be_in_first_position!
+    Product.find_each do |product|
+      product.update_attribute(:position, product.position + 1)
+    end
+    self.update_attribute(:position, 0)
+  end
+
 end
